dcbd65939534c3befe6d7c1f40758c0642f983e5
Eugene <eugene.bravov@gmail.com>
2025-04-01 18:14:09 -0300

    cleanup

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local obj={}
+local obj = {}
 obj.__index = obj
 
 -- Metadata
@@ -9,42 +9,46 @@ obj.homepage = 'https://github.com/fullpipe/Caps2Esc.spoon'
 obj.license = 'MIT - https://opensource.org/licenses/MIT'
 
 function obj:init()
-    self.onlyCtrlPressed = false
+    -- true if CapsCtrl is down
     self.ctrlPressed = false
-    self.log = hs.logger.new('Caps2Esc','debug')
+
+    -- true if only CapsCtrl is down
+    self.onlyCtrlPressed = false
+
+    self.log = hs.logger.new('Caps2Esc', 'debug')
     self.log.i('Initializing')
 
     self.flagHandler = hs.eventtap.new(
-        {hs.eventtap.event.types.flagsChanged},
+        { hs.eventtap.event.types.flagsChanged },
         function(event)
-            if event:getKeyCode() ~= 62 and ctrlPressed then
-                onlyCtrlPressed = false
+            if event:getKeyCode() ~= 62 and self.ctrlPressed then
+                self.onlyCtrlPressed = false
                 return
             end
 
             if event:getKeyCode() == 62 then
-                ctrlPressed = not ctrlPressed
+                self.ctrlPressed = not self.ctrlPressed
             end
 
-            if ctrlPressed and event:getFlags():containExactly({'ctrl'}) then
-                onlyCtrlPressed = true
+            if self.ctrlPressed and event:getFlags():containExactly({ 'ctrl' }) then
+                self.onlyCtrlPressed = true
             end
 
-            if event:getKeyCode() == 62 and not ctrlPressed and onlyCtrlPressed then
-                hs.eventtap.event.newKeyEvent('escape', true):post()
+            if event:getKeyCode() == 62 and not self.ctrlPressed and self.onlyCtrlPressed then
+                hs.eventtap.event.newKeyEvent(hs.keycodes.map.escape, true):post()
             end
 
-            if not ctrlPressed then
-                onlyCtrlPressed = false
+            if not self.ctrlPressed then
+                self.onlyCtrlPressed = false
             end
         end
     )
 
     self.keyDownHandler = hs.eventtap.new(
-        {hs.eventtap.event.types.keyDown},
-        function(event)
-            if ctrlPressed then
-                onlyCtrlPressed = false
+        { hs.eventtap.event.types.keyDown },
+        function()
+            if self.ctrlPressed and self.onlyCtrlPressed then
+                self.onlyCtrlPressed = false
             end
         end
     )
@@ -64,7 +68,6 @@ end
 function obj:stop()
     self.flagHandler:stop()
     self.keyDownHandler:stop()
-
 
     self.onlyCtrlPressed = false
     self.ctrlPressed = false
